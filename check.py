4b5cbe6e2b851d9438e8e5cbdf61a867fad37bdc
choijihee <jhjenite@naver.com>
2019-01-14 19:36:04 +0900

    Add files via upload

---
+++ check.py
@@ -0,0 +1,44 @@
+import os
+#import argparser
+import prac
+
+def ssh_chk(i):
+    #'i' is port number
+
+    command 
+
+    for a in i:
+        
+        
+        
+
+
+
+    
+    
+
+
+
+
+def main():
+    #parser.argparse.ArgumentParser()
+    #parser.add_argument("x", required=True)
+    #args = parser.parse_args()
+    
+    #if args.x:
+    #    print("turn on")
+
+
+    #prac.ip_a is parsed IP Address 
+    #print(prac.ip_a)
+    
+    chk(a)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+

922c73f0c4f5b2305b363f7ab405ddb0838ad1ba
choijihee <jhjenite@naver.com>
2019-01-16 18:58:28 +0900

    Add files via upload

--- project2/ISEEU_Scanner.py
+++ project2/ISEEU_Scanner.py
@@ -7,18 +7,44 @@ import ftp_check
 import ssh_check
 import telnet_check
 
+ip = '192.168.175.130'
+#port_info = [0,0,0,0]
+
 def Port_Scan(ip):
     #print(port_check.nmap(ip))
     print("Port_Scan!")
     return port_check.nmap(ip)
 
-def Ftp_Scan(ip, port):
-    print("FTP_Scan!")
-    return ftp_check.connect(ip, port)  # 1 - open, 0 - closed
+def Ftp_Scan(ip):
+    
+    print("FTP Scan!")
+    #Port_Scan(ip) # list
+    portlist = Port_Scan(ip)
+    
+    for i in range(0, len(portlist)):
+        a = ftp_check.connect(ip, str(portlist[i]))
+        if a == 0:  
+            print(portlist[i],' : CLOSED PORT')
 
-def Ssh_Scan(ip, port):
+        if a == 1:
+            print(portlist[i],' : OPEN PORT')
+            port_info[0] = 1
+    #return #ftp_check.connect(ip, port)  # 1 - open, 0 - closed
+
+def Ssh_Scan(ip):
     print("SSH_Scan!")
-    return ssh_check.connect(ip, port)  # 1 - open, 0 - closed
+    portlist = Port_Scan(ip)
+    
+    for i in range(0, len(portlist)):
+        a = ssh_check.connect(ip, str(portlist[i]))
+        if a == 0 :
+            print(portlist[i], ' : CLOSED PORT')
+
+        if a == 1 :
+            print(portlist[i], ' : OPEN PORT')
+            port_info[1] = 1
+
+    #return ssh_check.connect(ip, port)  # 1 - open, 0 - closed
 
 def Telnet_Scan(ip, port):
     print("Telnet_Scan!")
@@ -38,6 +64,7 @@ def Firmware_Scan(Path):
 
 
 #print(Port_Scan('192.168.175.130'))
-#print(Ftp_Scan('192.168.175.130','21')) #1 = open, 0 = closed
+Ssh_Scan('192.168.175.130') #1 = open, 0 = closed
+
 #print(Ssh_Scan('192.168.175.130','22')) 
 #print(Telnet_Scan('192.168.175.130','22'))
